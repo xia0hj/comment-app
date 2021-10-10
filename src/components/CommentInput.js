b2947af4ba7046f083f2c1dc37f9865c6711b364
xiaohj <719887163@qq.com>
2021-10-11 02:30:25 +0800

    用户名输入框失去焦点时保存到localStorage，首次渲染页面时读取localStorage

--- src/components/CommentInput.js
+++ src/components/CommentInput.js
@@ -1,4 +1,4 @@
-import React, { useState,useEffect,useRef } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 
 export default function CommentInput(props) {
 
@@ -15,7 +15,6 @@ export default function CommentInput(props) {
         setContent(event.target.value);
     };
 
-
     // 发布按钮将调用父组件传过来的onSubmit方法处理输入框的内容
     const handleSubmit = () => {
         if (props.onSubmit) {
@@ -26,17 +25,26 @@ export default function CommentInput(props) {
         }
     };
 
+    // 首次渲染后自动聚焦内容输入框和从localStorage获取username
     const textareaRef = useRef(null);
-    useEffect(()=>{
+    useEffect(() => {
+        setUsername(window.localStorage.getItem('username'));
         textareaRef.current.focus();
-    },[]);
+    }, []);
+
+    // 用户名输入框失去焦点时保存到localStorage
+    const handleUsernameBlur = (event) => {
+        window.localStorage.setItem('username', event.target.value);
+    };
+
+
 
     return (
         <div className='comment-input'>
             {/* 用户名及其输入框 */}
             <div className='comment-field'>
                 <span className='comment-field-name'>用户名：</span>
-                <div className='comment-field-input'><input value={username} onChange={handleUsernameChange} /></div>
+                <div className='comment-field-input'><input value={username} onChange={handleUsernameChange} onBlur={handleUsernameBlur} /></div>
             </div>
 
             {/* 评论内容及其输入框 */}
