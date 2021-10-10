aec73066f5003cf9f27e4c668cf690dd1f41fda4
xiaohj <719887163@qq.com>
2021-10-11 01:26:04 +0800

    "发布"按钮将子组件的用户名和评论内容传递给父组件

--- src/components/CommentApp.js
+++ src/components/CommentApp.js
@@ -1,10 +1,17 @@
 import CommentInput from "./CommentInput";
 import CommentList from "./CommentList";
 
+
 export default function CommentApp(props){
+
+
+    const handleSubmitComment = (obj)=>{
+        console.log(obj);
+    };
+
     return(
         <div className='wrapper'>
-            <CommentInput></CommentInput>
+            <CommentInput onSubmit={handleSubmitComment}></CommentInput>
             <CommentList></CommentList>
         </div>
     );
--- src/components/CommentInput.js
+++ src/components/CommentInput.js
@@ -1,3 +1,43 @@
-export default function CommentInput(props){
-    return <div>CommentInput</div>;
+import React,{useState} from 'react'
+
+export default function CommentInput(props) {
+
+    const [username, setUsername] = useState('');
+    const [content, setContent] = useState('');
+
+    const handleUsernameChange = (event)=>{
+        setUsername(event.target.value);
+    };
+    const handleContentChange = (event)=>{
+        setContent(event.target.value);
+    };
+    const handleSubmit = (event)=>{
+        if(props.onSubmit){
+            props.onSubmit({
+                username: username,
+                content: content,
+            });
+        }
+    };
+
+    return (
+        <div className='comment-input'>
+            {/* 用户名及其输入框 */}
+            <div className='comment-field'>
+                <span className='comment-field-name'>用户名：</span>
+                <div className='comment-field-input'><input value={username} onChange={handleUsernameChange} /></div>
+            </div>
+
+            {/* 评论内容及其输入框 */}
+            <div className='comment-field'>
+                <span className='comment-field-name'>评论内容：</span>
+                <div className='comment-field-input'><textarea value={content} onChange={handleContentChange} /></div>
+            </div>
+
+            {/* 发布按钮 */}
+            <div className='comment-field-button'>
+                <button onClick={handleSubmit}>发布</button>
+            </div>
+        </div>
+    );
 }
